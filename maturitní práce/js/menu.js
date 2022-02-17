eca023905fc056a948e226729ea883c51a84cd16
PetrDord <74857760+PetrDord@users.noreply.github.com>
2022-02-17 20:13:17 +0100

    Add files via upload

---
+++ maturitní práce/js/menu.js
@@ -0,0 +1,13 @@
+const nav= document.querySelector(".links");
+const toggle= document.querySelector(".toggel");
+
+toggle.addEventListener("click", () => {
+    const visibility= nav.getAttribute("data-visible");
+    if(visibility === "false"){
+        nav.setAttribute("data-visible", true);
+        toggle.setAttribute("aria-expanded", true);
+    }else{
+        nav.setAttribute("data-visible", false);
+        toggle.setAttribute("aria-expanded", false);
+    }
+})
