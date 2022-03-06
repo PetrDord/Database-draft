3d39972bb19f21896ed1d108b405d17e6e8730e2
PetrDord <74857760+PetrDord@users.noreply.github.com>
2022-03-06 22:09:56 +0100

    Add files via upload

--- js/menu.js
+++ js/menu.js
@@ -1,13 +1,14 @@
-const nav= document.querySelector(".links");
-const toggle= document.querySelector(".toggel");
-
-toggle.addEventListener("click", () => {
-    const visibility= nav.getAttribute("data-visible");
-    if(visibility === "false"){
-        nav.setAttribute("data-visible", true);
-        toggle.setAttribute("aria-expanded", true);
-    }else{
-        nav.setAttribute("data-visible", false);
-        toggle.setAttribute("aria-expanded", false);
+/* Ukládání class links do konstanty @menu */ 
+const menu= document.querySelector(".links");
+/* Ukládání class ham do konstanty @button */ 
+const button= document.querySelector(".ham");
+/* Funkce, když se klikne na tlačíto toggel */
+button.addEventListener("click", () => {
+    /* Ukádání atributu stav do konstanty @vis */
+    const vis = menu.getAttribute("stav");
+    /* Podmínka jestli bude vis rovno tak ji zněmí na true */
+    if(vis === "false"){
+        menu.setAttribute("stav", true);
+        button.setAttribute("expanded", true);
     }
 })
